756e6eca81cc01cb0dd0a65e6f57aa40b89b7207
Anugrah R <36835471+Ryuma12@users.noreply.github.com>
2021-01-19 16:55:24 +0700

    Initial commit

---
+++ app/src/main/java/com/test/notetutorial/NoteViewModel.java
@@ -0,0 +1,40 @@
+package com.test.notetutorial;
+
+import android.app.Application;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
+
+import java.util.List;
+
+public class NoteViewModel extends AndroidViewModel {
+    private NoteRepository repository;
+    private LiveData<List<Note>> allnote;
+
+    public NoteViewModel(@NonNull Application application) {
+        super(application);
+        repository = new NoteRepository(application);
+        allnote = repository.getAllNotes();
+    }
+
+    public  void insert(Note note){
+        repository.insert(note);
+    }
+
+    public  void update(Note note){
+        repository.update(note);
+    }
+
+    public  void delete(Note note){
+        repository.delete(note);
+    }
+
+    public  void deleteAllNotes(){
+        repository.deleteAllNote();
+    }
+
+    public LiveData<List<Note>> getAllnote() {
+        return allnote;
+    }
+}
